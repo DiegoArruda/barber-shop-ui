9df949e2d1183703a7fa01484778ce29f30b4ecc
Diego <arrudadiego1@gmail.com>
2025-03-27 06:45:45 -0300

    Dependency Injections

--- src/app/clients/new-client/new-client.component.ts
+++ src/app/clients/new-client/new-client.component.ts
@@ -1,8 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { SERVICES_TOKEN } from '../../services/service.token';
+import { ClientsService } from '../../services/api-client/clients/clients.service';
+import { IClientService } from '../../services/api-client/clients/iclients.service';
 
 @Component({
   selector: 'app-new-client',
   templateUrl: './new-client.component.html',
   styleUrl: './new-client.component.scss',
+  providers: [
+    { provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService },
+  ],
 })
-export class NewClientComponent {}
+export class NewClientComponent {
+  constructor(
+    @Inject(SERVICES_TOKEN.HTTP.CLIENT)
+    private readonly httpService: IClientService
+  ) {}
+}
