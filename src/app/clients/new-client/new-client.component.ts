7731f64f08a3b2848a14741f2e6b8b105a498c03
Diego <arrudadiego1@gmail.com>
2025-03-27 07:11:13 -0300

    Creating client subscription page

--- src/app/clients/new-client/new-client.component.ts
+++ src/app/clients/new-client/new-client.component.ts
@@ -1,7 +1,11 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, OnDestroy } from '@angular/core';
 import { SERVICES_TOKEN } from '../../services/service.token';
 import { ClientsService } from '../../services/api-client/clients/clients.service';
 import { IClientService } from '../../services/api-client/clients/iclients.service';
+import { ClientFormComponent } from '../components/client-form/client-form.component';
+import { ClientModelForm } from '../clients.models';
+import { Subscription } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-client',
@@ -10,10 +14,30 @@ import { IClientService } from '../../services/api-client/clients/iclients.servi
   providers: [
     { provide: SERVICES_TOKEN.HTTP.CLIENT, useClass: ClientsService },
   ],
+  imports: [ClientFormComponent],
 })
-export class NewClientComponent {
+export class NewClientComponent implements OnDestroy {
+  private httpSubscription?: Subscription;
+
   constructor(
     @Inject(SERVICES_TOKEN.HTTP.CLIENT)
-    private readonly httpService: IClientService
+    private readonly httpService: IClientService,
+    //@Inject(SERVICES_TOKEN.SNACKBAR)
+    //private readonly snackBarManager: ISnackbarManagerService,
+    private readonly router: Router
   ) {}
+
+  ngOnDestroy(): void {
+    if (this.httpSubscription) {
+      this.httpSubscription.unsubscribe();
+    }
+  }
+
+  onSubmitClient(value: ClientModelForm) {
+    const { id, ...request } = value;
+    this.httpSubscription = this.httpService.save(request).subscribe((_) => {
+      //this.snackBarManager.show('Usuário cadastrado com sucesso');
+      this.router.navigate(['clients/list']);
+    });
+  }
 }
